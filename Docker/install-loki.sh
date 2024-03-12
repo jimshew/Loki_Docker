5d2b54dac2b457f88446780fff457d3637ec90d1
James Shewmaker <james@bluenotch.com>
2024-03-12 02:59:21 -0400

    Use older dpkt, move to 0.3.1 packaging

--- Docker/install-loki.sh
+++ Docker/install-loki.sh
@@ -1,19 +1,22 @@
 #/bin/bash
 
-LOKI_FILE="loki_0.3.0-r502-5_amd64.deb"
+LOKI_FILE="loki_0.3.1_amd64.deb"
 
 APT_FLAGS="-q -y --no-install-recommends"
 DEBIAN_FRONTEND=noninteractive
 
 echo "* Install dependency"
 install_packages \
-	libdumbnet1 python-libpcap python-gtk2 python-dpkt python-dumbnet python-ipy python-glade2 python-urwid \
+	libdumbnet1 python-libpcap python-gtk2 python-dumbnet python-ipy python-glade2 python-urwid \
 	iptables bridge-utils \
 	ifupdown \
+	vim \
 	wget
 
+echo "* Get known-good dpkt"
+wget --quiet --no-check-certificate https://launchpad.net/ubuntu/+source/python-dpkt/1.8.r98-0.1/+build/8368756/+files/python-dpkt_1.8.r98-0.1_all.deb
 echo "* Get ${LOKI_FILE}"
-wget --quiet --no-check-certificate https://github.com/Raizo62/Loki_on_Kali/raw/master/${LOKI_FILE}
+wget --quiet --no-check-certificate https://github.com/jimshew/Loki_Docker/raw/master/${LOKI_FILE}
 echo "* Install ${LOKI_FILE}"
 dpkg -i ${LOKI_FILE}
 
